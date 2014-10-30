fe24b4c721a8178affc18dd4c8edf163e9303230
KyungRyun <kyung_ryun@naver.com>
2014-10-30 09:33:53 +0900

    Test

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a=0;
+		
+		a = input.nextInt();
+		System.out.println(a);
+
+	}
+
+}
